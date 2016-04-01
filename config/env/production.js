8be7f450ce4532a6a5214d63118470741999a34c
Mauricio Melo <mauricio@mauriciomelo.com.br>
2016-04-01 16:54:41 -0300

    Use google key eviroment variable in the production config

--- config/env/production.js
+++ config/env/production.js
@@ -1,6 +1,9 @@
+var googlePlacesApiKey = process.env.GOOGLE_PLACES_API_KEY || require('../secrets').googlePlacesApiKey;
+
+console.log(googlePlacesApiKey);
 module.exports = {
   db: {
     mongodb: 'mongodb://localhost:27017/simpatize-prod',
   },
-  googlePlacesApiKey: require('../secrets').googlePlacesApiKey
+  googlePlacesApiKey: googlePlacesApiKey
 };
